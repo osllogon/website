4d9670282339cb06ceb6036f14fa39d76a369c8d
Oscar Llorente Gonzalez <oscar.llorente.gonzalez@ericsson.com>
2025-03-18 11:53:10 +0100

    first finished version of the experience

--- website/components/cards.py
+++ website/components/cards.py
@@ -7,24 +7,59 @@ import reflex as rx
 
 
 def experience_card(
-    title: str, company: str, logo_path: str, duration: str, description: rx.Component
+    title: str, company: str, duration: str, logo_path: str, introduction: str, bullets: list[str]
 ) -> rx.Component:
+    """
+    This function creates a card for the experience section.
+
+    Returns:
+        Card for Experience section.
+    """
+    
     return rx.hstack(
-        rx.vstack(
+        rx.box(
             rx.center(
-                rx.image(src=logo_path, height="10em", justify_content="center"),  # Company logo
-                width="100%",
+                rx.image(
+                    src=logo_path,
+                    justify_content="center",
+                    aspect_ratio="initial",
+                    max_height="10em"
+                ),
             ),
-            width="30%"
+            width="25%"
         ),
         rx.vstack(
             rx.heading(title, size="4"),
-            rx.text(company, font_weight="bold", color="gray"),
-            description,
+            rx.text(f"{company}, {duration}", font_weight="bold", color="gray"),
+            get_description_experience(introduction, bullets),
             align="start",
+            width="75%"
         ),
         width="50%",
         spacing="9",
         border_top=f"1.5px solid {rx.color('violet', 6)}",
         padding="2%",
+        align_items="center",
+    )
+    
+
+def get_description_experience(introduction: str, bullets: list[str]) -> rx.Component:
+    """
+    This function defines a description to be included in an experience 
+    card.
+
+    Returns:
+        Vertical stack with a brief description and a list of bullets 
+            to include.
+    """
+    
+    return rx.vstack(
+        rx.text(introduction),
+        rx.list.unordered(  # type: ignore
+            *[rx.list.item(bullet) for bullet in bullets], # type: ignore
+            list_style_type="circle",
+            style={  # type: ignore
+                "text_align": "justify",
+            },
+        ),
     )
--- website/pages/experience.py
+++ website/pages/experience.py
@@ -10,7 +10,7 @@ from website.components.menu import get_menu
 from website.components.cards import experience_card
 
 
-@rx.page(route="/experience", title="Oscar Llorente | Home")
+@rx.page(route="/experience", title="Oscar Llorente | Experience")
 def experience_page() -> rx.Component:
     """
     This function defines the home page of the website.
@@ -25,87 +25,113 @@ def experience_page() -> rx.Component:
             rx.box(height="2em"),
             experience_card(
                 "Senior Data Scientist",
-                "Ericsson, Jan 2024 - Present",
-                "/images/logo.png",
+                "Ericsson",
                 "Jan 2024 - Present",
-                rx.vstack(
-                    rx.text(
-                        "Data Scientist at Ericsson Cognitive Network Solutions (JS6):"
-                    ),
-                    rx.list.unordered(  # type: ignore
-                        rx.list.item(  # type: ignore
-                            "Member of the Data Science Board, group of "
-                            "experts who manage Machine Learning in the organization."
-                        ),
-                        rx.list.item(  # type: ignore
-                            "Head of the Geometric Artificial Intelligence (GAI) Lab."
-                        ),
-                        rx.list.item(  # type: ignore
-                            "Machine Learning Lead of the Uplink Optimizer 4G and 5G, "
-                            "telecommunications network optimizer that improves the "
-                            "uplink interference with Graph Neural Networks."
-                        ),
-                        list_style_type="circle",
-                        style={  # type: ignore
-                            "text_align": "justify",
-                        },
-                    ),
-                ),
+                "/images/ericsson_logo.png",
+                "Data Scientist at Ericsson Cognitive Network Solutions (JS6):",
+                [
+                    "Member of the Data Science Board, group of experts who manage "
+                    "Machine Learning in the organization.",
+                    "Head of the Geometric Artificial Intelligence (GAI) Lab.",
+                    "Machine Learning Lead of the Uplink Optimizer 4G and 5G, "
+                    "telecommunications network optimizer that improves the "
+                    "uplink interference with Graph Neural Networks."
+                ]
             ),
             experience_card(
                 "Adjunct Professor",
-                "Comillas Pontifical University (ICAI), Jan 2024 - Present",
-                "/images/icai_logo.png",
+                "Comillas Pontifical University (ICAI)",
                 "Jan 2024 - Present",
-                rx.vstack(
-                    rx.text(
-                        "Data Scientist at Ericsson Cognitive Network Solutions (JS6):"
-                    ),
-                    rx.list.unordered(  # type: ignore
-                        rx.list.item(  # type: ignore
-                            "Member of the Data Science Board, group of "
-                            "experts who manage Machine Learning in the organization."
-                        ),
-                        rx.list.item(  # type: ignore
-                            "Head of the Geometric Artificial Intelligence (GAI) Lab."
-                        ),
-                        rx.list.item(  # type: ignore
-                            "Machine Learning Lead of the Uplink Optimizer 4G and 5G, "
-                            "telecommunications network optimizer that improves the "
-                            "uplink interference with Graph Neural Networks."
-                        ),
-                        list_style_type="circle",
-                        style={  # type: ignore
-                            "text_align": "justify",
-                        },
-                    ),
-                ),
+                "/images/icai_logo.png",
+                "Bachelor of Engineering, Mathematical Engineering & Artificial Intelligence (iMAT):",
+                [
+                    "Computer Vision II, Theory & Lab Professor (2024).",
+                    "Deep Learning, Theory & Lab Professor (2024-2025).",
+                    "Natural Language Processing (NLP), Lab Professor (2024)."
+                ]
             ),
             experience_card(
                 "Experienced Data Scientist",
-                "Ericsson, Jun 2023 - Dec 2023",
-                "/images/logo.png",
-                "Jan 2024 - Present",
-                rx.vstack(
-                    rx.text(
-                        "Data Scientist at Ericsson Cognitive Network Solutions (JS5):"
-                    ),
-                    rx.list.unordered(  # type: ignore
-                        rx.list.item(  # type: ignore
-                            "Machine Learning Lead of the Uplink Optimizer 4G and 5G, "
-                            "telecommunications network optimizer that improves the "
-                            "uplink interference with Graph Neural Networks."
-                        ),
-                        rx.list.item(  # type: ignore
-                            "Creation of a internal library to handle datasets from S3 "
-                            "bucket and AWS based in Boto3."
-                        ),
-                        list_style_type="circle",
-                        style={  # type: ignore
-                            "text_align": "justify",
-                        },
-                    ),
-                ),
+                "Ericsson",
+                "Jun 2023 - Dec 2023",
+                "/images/ericsson_logo.png",
+                "Data Scientist at Ericsson Cognitive Network Solutions (JS5):",
+                [
+                    "Machine Learning Lead of the Uplink Optimizer 4G and 5G, "
+                    "telecommunications network optimizer that improves the uplink "
+                    "interference with Graph Neural Networks.",
+                    
+                ]
+            ),
+            experience_card(
+                "Data Scientist",
+                "Ericsson",
+                "Jun 2022 - May 2023",
+                "/images/ericsson_logo.png",
+                "Data Scientist at Ericsson Cognitive Network Solutions (JS4):",
+                [
+                    "Data Scientist in Uplink Optimizer, telecommunications network "
+                    "optimizer that improves the uplink interference with Graph Neural "
+                    "Networks.",
+                    "Expert Data Scientist in Graph Neural Networks."
+                    
+                ]
+            ),
+            experience_card(
+                "Data Scientist Intern",
+                "Ericsson",
+                "Sep 2021 - May 2022",
+                "/images/ericsson_logo.png",
+                "Data Scientist Intern at Ericsson:",
+                [
+                    "Development and implementation of a NLP projects, document "
+                    "similarity between internal inquiries to save time to other "
+                    "departments",
+                    "Research project about detection of cybersecurity vulnerabilities "
+                    "based on packages descriptions "
+                    
+                ]
+            ),
+            experience_card(
+                "Software Engineer Intern",
+                "Stemy Energy",
+                "Sep 2020 - March 2021",
+                "/images/stemy_logo.png",
+                "Software Engineer Intern at Stemy Energy:",
+                [
+                    "Backend and frontend development using Django framework."
+                    
+                ]
+            ),
+            experience_card(
+                "Data Scientist Intern",
+                "Management Solutions",
+                "Jul 2020 - Aug 2020",
+                "/images/management_solutions.svg",
+                "Data Scientist Intern at Management Solutions:",
+                [
+                    "development of an Auto Machine Learning tool."
+                    
+                ]
+            ),
+            experience_card(
+                "Business Process Management Engineer Intern",
+                "Telefonica",
+                "Jan 2020 - Jun 2020",
+                "/images/telefonica_logo.png",
+                "Member of the Automation Lab. The mains tasks of the team were to "
+                "analyze and develop automation tools for different departments inside "
+                "Telefónica.",
+                []
+            ),
+            experience_card(
+                "Software Engineer Intern",
+                "Institute of Research in Technology (IIT)",
+                "Jun 2019 - Dec 2019",
+                "/images/icai_logo.png",
+                "Intern for the development of an optimizer tool for filling containers"
+                " and trucks.",
+                []
             ),
             width="100%",
             spacing="0",
